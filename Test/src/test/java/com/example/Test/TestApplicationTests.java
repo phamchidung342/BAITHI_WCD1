e26f997d3b8f5b0acd3521ea2b044a1c0c86d6c6
phamchidung342 <63303733+phamchidung342@users.noreply.github.com>
2025-04-19 10:33:04 +0700

    Initial commit

---
+++ Test/src/test/java/com/example/Test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test;
+
+import 	org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}	
+
+}
